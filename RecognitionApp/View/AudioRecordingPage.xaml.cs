0fd6b85e82253395f7034a0441ec8102238e0d95
Maikled <zyulyaev12@gmail.com>
2024-05-14 23:01:01 +0400

    Добавлена запись аудио

--- RecognitionApp/View/AudioRecordingPage.xaml.cs
+++ RecognitionApp/View/AudioRecordingPage.xaml.cs
@@ -1,10 +1,12 @@
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
+using NAudio.Wave;
 using RecognitionApp.Model;
 using RecognitionApp.Model.Enums;
 using RecognitionApp.ViewModel;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using Windows.Storage;
 
@@ -15,6 +17,8 @@ namespace RecognitionApp.View
     {
         private AudioRecordingViewModel _viewModel { get; set; } = new AudioRecordingViewModel();
         private GeneralViewModel _generalViewModel { get; set; }
+        private WaveIn _waveIn { get; set; }
+        private WaveFileWriter _waveFileWriter { get; set; }
 
         public AudioRecordingPage()
         {
@@ -43,9 +47,44 @@ namespace RecognitionApp.View
         }
 
         [RelayCommand]
-        public async Task RecordingAudioFile()
+        public void RecordingAudioStart()
         {
+            if(_waveIn == null)
+            {
+                _waveIn = new WaveIn();
+                _waveIn.WaveFormat = new WaveFormat(16000, 2);
+                _waveIn.DataAvailable += _waveIn_DataAvailable;
+            }
+
+            var filePath = Path.Combine(ApplicationData.Current.TemporaryFolder.Path, $"recording-{Guid.NewGuid()}.wav");
+            _waveFileWriter = new WaveFileWriter(filePath, _waveIn.WaveFormat);
             
+            _waveIn.StartRecording();
+        }
+
+        [RelayCommand]
+        public async Task RecordingAudioStop()
+        {
+            _waveIn?.StopRecording();
+
+            if(_waveFileWriter != null)
+            {
+                _waveIn.Dispose();
+                _waveIn = null;
+                _waveFileWriter.Close();
+
+                var storageFile = await StorageFile.GetFileFromPathAsync(_waveFileWriter.Filename);
+                AddFileRecognition(storageFile);
+
+                _waveFileWriter = null;
+                _waveFileWriter.Dispose();
+            }
+        }
+
+        private void _waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            _waveFileWriter.Write(e.Buffer, 0, e.BytesRecorded);
+            recordingTimeTextBlock.Text = _waveFileWriter.TotalTime.ToString();
         }
 
         private async void AddFileRecognition(StorageFile storageFile)
